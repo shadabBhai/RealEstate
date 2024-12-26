39d63e3a2c50ef316a03b1f5f43c8c0ee43b422e
shadabBhai <iamshadabkhan3@github.com>
2024-12-26 20:52:51 +0530

    Update : file structure

---
+++ app/(roots)/(tabs)/index.tsx
@@ -0,0 +1,23 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-blue-300 mb-4 font-rubikExtraBold text-7xl  ">
+        {" "}
+        Real Estate
+      </Text>
+      <Link href="/signIn"> sign In</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/1">Property</Link>
+    </View>
+  );
+}
